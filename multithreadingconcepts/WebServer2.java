9c510cae1a08139fcee606efe5e6eddedad6c55f
LokeshSylada <94172803+LokeshSylada@users.noreply.github.com>
2021-12-07 23:36:31 +0530

    Update WebServer2.java

--- multithreadingconcepts/WebServer2.java
+++ multithreadingconcepts/WebServer2.java
@@ -6,8 +6,7 @@ public class WebServer2 implements Runnable {
 	public void run() {
 		Thread.currentThread().setName("WebServer2");
 		for (int i = 0; i < 5; i++) {
-			System.out
-					.println("Hi there, hello from " + Thread.currentThread().getName() + " for the " + i + "th time");
+			System.out.println("Hi there, hello from " + Thread.currentThread().getName() + " for the " + i + "th time");
 		}
 
 	}
